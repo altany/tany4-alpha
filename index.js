26c075146b91617dc96e4b53080bcc942269833c
Tania <taniapapazaf@gmail.com>
2016-12-09 19:40:35 +0000

    set up the sass

--- index.js
+++ index.js
@@ -6,10 +6,25 @@ app.set('port', (process.env.PORT || 5000));
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-  res.render('index')
-})
+  res.render('index', {page:'home'});
+});
+
+
+var sass = require('node-sass');
+
+app.get('/public/style.css', function(req, res, next) {
+	sass.render({
+	  file: 'public/sass/style.sass',
+	  sourceMapEmbed: true
+	}, function(err, result) {
+	  if (err) return next(err);
+	  res.set('Content-Type', 'text/css').send(result.css);
+	});
+});
 
 app.listen(app.get('port'), function () {
-  console.log('Example app listening on port ' + app.get('port'));
-})
+  console.log('App listening on port ' + app.get('port'));
+});
