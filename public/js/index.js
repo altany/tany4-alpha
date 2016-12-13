0d4ca6045fd7d77818d62f02fc8a8dd6563dc32a
Tania <taniapapazaf@gmail.com>
2016-12-13 08:38:04 +0000

    add js (empty)

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+	
+	/*var mainContentHeight = $('.wrapper').outerHeight()-$('footer').outerHeight() - $('header').outerHeight();
+	console.log(mainContentHeight);
+    if ($('.pageContent').outerHeight() < mainContentHeight) {
+		$('.pageContent').css('margin-top', (mainContentHeight - $('.pageContent').height())/4)
+	}*/
+});
