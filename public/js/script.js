5746fb32fd10fc84777a26ce484c0f7beda4fd18
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2016-12-29 17:25:11 +0000

    improve responsiveness and add to social

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,14 @@
 $( document ).ready(function() {
 	
-    $('body').addClass(isTouchDevice()?'touch':'no-touch');
+	$('body').addClass(isTouchDevice()?'touch':'no-touch');
 	/*var mainContentHeight = $('.wrapper').outerHeight()-$('footer').outerHeight() - $('header').outerHeight();
 	console.log(mainContentHeight);
-    if ($('.pageContent').outerHeight() < mainContentHeight) {
+	if ($('.pageContent').outerHeight() < mainContentHeight) {
 		$('.pageContent').css('margin-top', (mainContentHeight - $('.pageContent').height())/4)
 	}*/
 });
 
 function isTouchDevice() {
-  return 'ontouchstart' in window        // works on most browsers 
-      || navigator.maxTouchPoints;       // works on IE10/11 and Surface
+	return 'ontouchstart' in window        // works on most browsers 
+		|| navigator.maxTouchPoints;       // works on IE10/11 and Surface
 };
