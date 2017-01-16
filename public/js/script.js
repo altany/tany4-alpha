7fca96ae7deffbb8ce04e03cb0c9a6f113e91433
Tania <taniapapazaf@gmail.com>
2017-01-16 22:45:51 +0000

    remove extra semicolon

--- public/js/script.js
+++ public/js/script.js
@@ -6,4 +6,4 @@ $( document ).ready(function() {
 function isTouchDevice() {
 	return 'ontouchstart' in window        // works on most browsers 
 		|| navigator.maxTouchPoints;       // works on IE10/11 and Surface
-};
+}
