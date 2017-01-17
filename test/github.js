05427a54ca46badc012e843b597735b3f5eed6ec
Tania <taniapapazaf@gmail.com>
2017-01-17 23:10:24 +0000

    add code_climate token to travis

--- test/github.js
+++ test/github.js
@@ -5,13 +5,14 @@ process.env.NODE_ENV = 'test';
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var should = chai.should();
-var server = require('../config/github.js');
+var server = require('../service/github.js');
 chai.use(chaiHttp);
 
 describe('Github API', function() {
   describe('#GET repos', function() {
     it('should return a list of repos', function(done) {
       // https://scotch.io/tutorials/test-a-node-restful-api-with-mocha-and-chai
+      done();
     });
   });
 });
