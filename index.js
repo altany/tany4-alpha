f51fa6302c8d119aece10dfd784aa6b65bdafea4
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2017-01-17 11:50:30 +0000

    Tidy up code according to code climate suggestions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var async = require('async');
 var marked = require('marked');
 var timeAgo = require('node-time-ago');
 var ua = require('universal-analytics');
-
+var sass = require('node-sass');
 
 var httpRequestCache = new RequestHttpCache({
 	max: 10*1024*1024, // Maximum cache size (1mb) defaults to 512Kb 
@@ -25,7 +25,7 @@ var clientID = process.env.GITHUB_CLIENTID;
 var clientSecret = process.env.GITHUB_SECRET;
 var gaID = process.env.GA_ACCOUNT_ID;
 
-var visitor = ua(gaID); //.debug(); To log the tracking info for testing
+var visitor = ua(gaID).debug(); //To log the tracking info for testing
 
 marked.setOptions({
 	renderer: new marked.Renderer(),
@@ -62,23 +62,10 @@ app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-	
-	// Track pageview
-	visitor.pageview({dp: "/", dt: "Home"}).send();
-	
-	res.render('index', {page:'home', ldJson: ldJson});
-});
+/* Host my CV as pdf */
+app.use('/TaniaPapazafeiropoulou-CV', express.static(path.join(__dirname, '/public/files/TaniaPapazafeiropoulouCV.pdf')));
 
-app.get('/social', function (req, res) {
-	
-	// Track pageview
-	visitor.pageview({dp: "/social", dt: "Social"}).send();
-	
- 	res.render('social', {page:'social', ldJson: ldJson});
-});
-var sass = require('node-sass');
-
+/* Compile my sass to css via connect middleware */
 app.get('/public/style.css', function(req, res, next) {
 	sass.render({
 	  file: 'public/sass/style.sass',
@@ -89,76 +76,82 @@ app.get('/public/style.css', function(req, res, next) {
 	});
 });
 
-/* Host my CV as pdf */
-app.use('/TaniaPapazafeiropoulou-CV', express.static(path.join(__dirname, '/public/files/TaniaPapazafeiropoulouCV.pdf')));
-
-app.get('/github', function(req, res, next) {
+app.get('/:page?', function (req, res) {
+	console.log(req.params.page);
+	var page = req.params.page?req.params.page:'home';
 	
 	// Track pageview
-	visitor.pageview({dp: "/github", dt: "Github"}).send();
+	visitor.pageview({dp: '/' + (req.params.page?req.params.page:''), dt: page.charAt(0).toUpperCase() + page.slice(1)}).send();
 	
-	var gitHost = 'https://api.github.com';
-	var apiVersion = 'v3';
-	var options = {
-		url: gitHost + '/users/altany/repos?sort=created&client_id=' + clientID + '&client_secret=' + clientSecret,
-		headers: {
-			'User-Agent': 'altany'
-		}
-	};
 	
-	request(options, function (error, response, body) {
-		if (error) return next(new Error (error));
-		var repos = JSON.parse(body);
-		if (repos.length) {
-			async.each( repos, function(repo, callback){
-				options.url = gitHost + '/repos/' + repo.full_name + '/contents/README.md?client_id=' + clientID + '&client_secret=' + clientSecret;
-				options.headers['Accept'] = 'application/vnd.github.' + apiVersion + '.raw';
-				request(options, function (error, response, body) {
-					if (error) return callback(error);
-					if (response.statusCode===404) {
-						repo.readme = 'No description available...'
-					}
-					else if (response.statusCode!==200) {
-						console.warn('Error getting README content for', repo.name);
-						repo.readme = 'There was an error while getting this repo\'s README file';
-					}
-					else {
-						repo.readme = marked(body.toString());
-					}
-					options.url = gitHost + '/repos/altany/' + repo.name + '/commits?client_id=' + clientID + '&client_secret=' + clientSecret;
-					request(options, function (e, r, b) {
-						if(e) return callback(e);
-						var commit = JSON.parse(b)[0];
-						repo.lastCommit = {};
-						if (r.statusCode!==200) {
-							console.warn('Error getting the commits list for', repo.name);
-							repo.lastCommit = {
-								message: 'Error getting the commit history'
-							}
+	// If page is Github, render the Github API results
+	if (req.params.page && req.params.page === 'github') {
+		var gitHost = 'https://api.github.com';
+		var apiVersion = 'v3';
+		var options = {
+			url: gitHost + '/users/altany/repos?sort=created&client_id=' + clientID + '&client_secret=' + clientSecret,
+			headers: {
+				'User-Agent': 'altany'
+			}
+		};
+
+		request(options, function (error, response, body) {
+			if (error) return next(new Error (error));
+			var repos = JSON.parse(body);
+			if (repos.length) {
+				async.each( repos, function(repo, callback){
+					options.url = gitHost + '/repos/' + repo.full_name + '/contents/README.md?client_id=' + clientID + '&client_secret=' + clientSecret;
+					options.headers['Accept'] = 'application/vnd.github.' + apiVersion + '.raw';
+					request(options, function (error, response, body) {
+						if (error) return callback(error);
+						if (response.statusCode===404) {
+							repo.readme = 'No description available...'
+						}
+						else if (response.statusCode!==200) {
+							console.warn('Error getting README content for', repo.name);
+							repo.readme = 'There was an error while getting this repo\'s README file';
 						}
 						else {
-							repo.lastCommit = {
-								link: commit.html_url,
-								date: timeAgo(commit.commit.author.date),
-								message: commit.commit.message
-							}
+							repo.readme = marked(body.toString());
 						}
-						callback()
+						options.url = gitHost + '/repos/altany/' + repo.name + '/commits?client_id=' + clientID + '&client_secret=' + clientSecret;
+						request(options, function (e, r, b) {
+							if(e) return callback(e);
+							var commit = JSON.parse(b)[0];
+							repo.lastCommit = {};
+							if (r.statusCode!==200) {
+								console.warn('Error getting the commits list for', repo.name);
+								repo.lastCommit = {
+									message: 'Error getting the commit history'
+								}
+							}
+							else {
+								repo.lastCommit = {
+									link: commit.html_url,
+									date: timeAgo(commit.commit.author.date),
+									message: commit.commit.message
+								}
+							}
+							callback()
+						});
 					});
+				}, function(err){
+					if (err) return next(new Error('Failed while reading one of the repos:' + err))
+					return res.render('github', {page: 'github', repos: repos});
 				});
-			}, function(err){
-				if (err) return next(new Error('Failed while reading one of the repos:' + err))
-				return res.render('github', {page: 'github', repos: repos});
-			});
-		}
-		else {
-			var message = 'No repos found right now. Please try again later';
-			console.warn(message);
-			return res.render('github', {page: 'github', warning: message})
-		}
-		
-	});
+			}
+			else {
+				var message = 'No repos found right now. Please try again later';
+				console.warn(message);
+				return res.render('github', {page: 'github', warning: message})
+			}
 
+		});
+	}
+	else { // Not Github
+		res.render((req.params.page?req.params.page:'index'), {page:page, ldJson: ldJson});
+	}
+	
 });
 
 app.listen(app.get('port'), function () {
