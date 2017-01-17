195892600e6fe05693c3704cde7cc4c4e898699e
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2017-01-17 15:40:55 +0000

    simplify names

--- config/github.js
+++ config/github.js
@@ -1,12 +1,11 @@
 var githubApiConfig = {
 	clientID: process.env.GITHUB_CLIENTID,
 	clientSecret: process.env.GITHUB_SECRET,
-	gitHost: 'https://api.github.com'
+	host: 'https://api.github.com'
 };
 
-githubApiConfig.authParam = 'client_id=' + githubApiConfig.clientID + '&client_secret=' + githubApiConfig.clientSecret;
+githubApiConfig.auth = 'client_id=' + githubApiConfig.clientID + '&client_secret=' + githubApiConfig.clientSecret;
 githubApiConfig.options = {
-	url: githubApiConfig.gitHost + '/users/altany/repos?sort=created&' + githubApiConfig.authParam,
 	headers: {
 		'User-Agent': 'altany'
 	}
