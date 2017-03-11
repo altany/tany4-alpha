5f6f20a11a1ec44b5674934500e92d8b85f12b2c
Tania <taniapapazaf@gmail.com>
2017-03-11 22:53:05 +0000

    update config for webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,75 @@
-module.exports = process.env.NODE_ENV === 'production' ? require('./webpack.config/production.js') : require('./webpack.config/base.js');
+let path = require('path');
+var webpack = require('webpack');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+let config = {
+  context: path.join(__dirname, 'src'),
+  output: {
+    filename: 'bundle.js',
+    publicPath: path.join(__dirname, 'www')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx|\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.sass$/,
+        //loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+        use: ExtractTextPlugin.extract({
+           use:"css-loader!sass-loader",
+           fallback: 'style-loader'
+        })
+      },
+      {
+        test: /\.(eot|svg|ttf|woff)/,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]'
+        }
+      }
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename:'style.css',
+      disable: false,
+      allChunks: true
+    })
+  ],
+  resolve: {
+    modules: [
+      path.join(__dirname),
+      "node_modules"
+    ]
+  }
+};
+
+switch (process.env.NODE_ENV) {
+  case 'dev':
+    config.entry= {
+      app: [
+        'webpack-hot-middleware/client',
+        './js/main.js'
+      ]
+    };
+    config.devtool= 'cheap-module-source-map';
+    config.output.path= '/';
+    config.plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    );
+  break;
+  case 'production':
+  default:
+  config.entry= {
+    app: [
+      './js/main.js'
+    ]
+  }
+  config.devtool= 'source-map';
+  config.output.path= path.join(__dirname, 'www');
+}
+module.exports = config;
