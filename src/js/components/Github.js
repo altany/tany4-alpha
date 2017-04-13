6205c0cd4d5efb1aa75ede3936957745380e28c5
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2017-04-13 21:56:43 +0100

    fix typo

--- src/js/components/Github.js
+++ src/js/components/Github.js
@@ -13,7 +13,7 @@ class Github extends React.Component {
           </a>
         </p>
 
-        <h2>Github Api Playgound</h2>
+        <h2>Github Api Playground</h2>
 
         <p>
           {'This page contains a list of my Github repos. All the data is retrieved through the '}
